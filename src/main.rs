8415affc283df9a42e70e28911986cdae55a4117
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-22 16:47:04 +0100

    fix: allow package mutation arguments

--- src/main.rs
+++ src/main.rs
@@ -1661,7 +1661,7 @@ fn parse_cli(
     if matches!(command, CliCommand::Test) && native_requested {
         return Err("Nano: --native não é válido com 'test'".into());
     }
-    if matches!(command, CliCommand::PackageInit | CliCommand::PackageAdd | CliCommand::PackageRemove | CliCommand::PackageLock | CliCommand::PackageVerify | CliCommand::New | CliCommand::Fmt | CliCommand::Lsp | CliCommand::Repl | CliCommand::Debug)
+    if matches!(command, CliCommand::PackageInit | CliCommand::PackageLock | CliCommand::PackageVerify | CliCommand::New | CliCommand::Fmt | CliCommand::Lsp | CliCommand::Repl | CliCommand::Debug)
         && (native_requested || selected_backend.is_some() || selected_dtype.is_some() || output.is_some()) {
         return Err("Nano: opções não são válidas para este comando".into());
     }
